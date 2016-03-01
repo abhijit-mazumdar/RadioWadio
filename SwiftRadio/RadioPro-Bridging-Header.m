e2a0b8c442baf9b8ab24a9caf4444dc6e2cb565d
Abhijit Mazumdar <pankumazumdar@gmail.com>
2016-03-01 18:27:33 +0530

    Refactor

--- SwiftRadio/RadioPro-Bridging-Header.m
+++ SwiftRadio/RadioPro-Bridging-Header.m
@@ -1,10 +1,4 @@
-//
-//  RadioPro-Bridging-Header.m
-//  RadioPro
-//
-//  Created by Matthew Fecher on 3/22/15.
-//  Copyright (c) 2015 CodeMarket.io. All rights reserved.
-//
+
 
 #import "RadioPro-Bridging-Header.h"
 
